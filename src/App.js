21e025800776d41cfaa2138eb7345ad38bdd4655
ranraok <46853842+ranraok@users.noreply.github.com>
2020-02-28 13:58:38 -0500

    almost done, just need to finish the search function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
+import {Route} from 'react-router-dom';
+import CharacterList from "./components/CharacterList.js";
+import Character from "./components/Character.js";
+import SearchForm from "./components/SearchForm.js";
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+        <Header />
+        <SearchForm/>
+        <Route path="/" render={() => <CharacterList />} />
+        <Route path='/Character:id' render = {() => <Character/>} />
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,51 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 export default function SearchForm() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [characters, setCharacters] = useState([]);
+
+ 
+  const character = () => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character')
+    .then(response =>{
+      setCharacters(response.data.results);
+    })
+  }
+
+  useEffect(() => {
+
+    const results = characters.filter(character => 
+      character.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResults(results);
+  }, [searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
  
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <label htmlFor="name">Search:</label>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </form>
+      <div className="character-list">
+        <ul>
+        {searchResults.map(character => (
+          <li key={character}>{character}</li>
+        ))}
+        </ul>
+      </div>
     </section>
   );
 }
